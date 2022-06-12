7e8b48a0d79aa87347a9a0490b3b65ee0e15fefd
gitgroch <geoff.rochford@gmail.com>
2022-06-12 14:03:01 +0000

    Added code for getNewQuestion function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,27 @@
+// Wait for the DOM to finish loading before starting the survey 
+
+document.addEventListener("DOMContentLoaded", function(){
+
+})
+
 // Global Variables 
 
 const question = document.getElementById('question');
 const choices = document.getElementsByClassName('.choice-text');
-let score = ;
 
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+const score_points = {
+    if 
+};
+
+const max_questions = 5;
+
+// Survey Questions 
 let questions = [
     {
         question:'Who is the greatest historical figure in the history of the Galaxy?',
@@ -22,7 +40,7 @@ let questions = [
     {
         question:'You come across an unidentified starship having quantum engine trouble in the Delta Quadrant. Do you:',
         choice1: 'Offer the crew use of your holodeck while they wait for Galactic AA to show up',
-        choice2: 'Slow down to propulsion engines as you pass by but don’t stop',
+        choice2: 'Slow down to propulsion engines as you pass by but dont stop',
         choice3: 'Offer to run statistical analysis of the quantum relay sub routines in a fraction of a zeptosecond',
         choice4: 'Capture the ship and crew for the glory of the home world',
     },
@@ -36,9 +54,45 @@ let questions = [
     {
         question:'Are you an Android?',
         choice1: '01101110 01101111',
-        choice2: 'I don’t read binary',
+        choice2: 'I dont read binary',
         choice3: '01111001 01100101 01110011',
         choice4: 'I have no need for your puny human numbers',
     },
 ]
+
+
+
+// Functions 
+function startSurvey() {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions]
+    getNewQuestion();
+}
+
+function getNewQuestion() {
+    if(availableQuestions.lenght === 0 || questionCounter > max_questions)
+    
+    return window.location.assign('/end.html')
+
+    questionCounter++;
+    
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.lenght);
+    currentQuestion = availableQuestions[questionsIndex];
+    question.InnerText = currentQuestion.question;
+}
+
+
+function checkAnswer() {
+
+}
+
+function storeAnswer() {
+
+}
+
+function calculateScore() {
+
+}
+
 
